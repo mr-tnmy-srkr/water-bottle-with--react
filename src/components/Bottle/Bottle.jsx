08b2ad61579c1b82f54b2039ee0ccc11efcc21f7
Tanmoy Sarkar <mr.tnmy.srkr@gmail.com>
2023-09-11 20:14:42 +0530

    added item to cart

--- src/components/Bottle/Bottle.jsx
+++ src/components/Bottle/Bottle.jsx
@@ -1,11 +1,15 @@
-const Bottle = ({ bottle }) => {
+/* eslint-disable react/prop-types */
+const Bottle = ({ bottle ,handleAddToCart}) => {
   // console.log(bottle);
   const { name, img, price } = bottle;
   return (
-    <div className="text-2xl ">
-      <h3>Bottle : {name}</h3>
-      <img src={img} alt="" />
-      <p>Price : ${price}</p>
+    <div className="text-2xl text-center space-y-4 border-4 border-sky-500 rounded-xl">
+      <h3 className="my-4">Bottle : {name}</h3>
+      <img className="w-11/12 mx-auto rounded-xl" src={img} alt="" />
+      <p className="text-red-900 ">Price : ${price}</p>
+      <div className="pb-4">
+      <button onClick={handleAddToCart} className="btn btn-primary capitalize">Purchase</button>
+      </div>
     </div>
   );
 };
--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -5,6 +5,9 @@ import Bottle from "../Bottle/Bottle";
 const Bottles = () => {
   const [bottles, setBottles] = useState([]);
 
+  //cart parent to child (need to focus)
+  const [cart, setCart] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -21,15 +24,29 @@ const Bottles = () => {
     fetchData();
   }, []);
 
+  //cart parent to child (need to focus)
+  const handleAddToCart = (bottle) => {
+    // console.log(bottle);
+    const newCart = [...cart, bottle];
+    setCart(newCart);
+  };
+
   return (
-  <>
-        <h2 className="text-4xl mb-6 text-blue-900">Bottles Here : {bottles.length}</h2>
-    <div className="grid grid-cols-3 gap-6">
-      {bottles.map((bottle) => (
-        <Bottle key={bottle.key} bottle={bottle}></Bottle>
-      ))}
-    </div>
-  </>
+    <>
+      <h2 className="text-4xl mb-6 text-primary ">
+        Bottles Available : {bottles.length}
+      </h2>
+      <h4 className="mb-5 text-2xl text-secondary">Cart : {cart.length}</h4>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
+        {bottles.map((bottle) => (
+          <Bottle
+            key={bottle.id}
+            bottle={bottle}
+            handleAddToCart={() => handleAddToCart(bottle)}
+          ></Bottle>
+        ))}
+      </div>
+    </>
   );
 };
 
